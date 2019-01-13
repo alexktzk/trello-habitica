a074a6c2f249c131ad7ed48bc64f5c1dbf2b6792
Alex Kutuzaki <alexktzk@gmail.com>
2019-01-13 22:35:41 +0300

    feat: prevent duplicated http requests

--- public/js/habitica.js
+++ public/js/habitica.js
@@ -1,60 +1,68 @@
 var h = habitica = ({
+  loading: {},
   api: 'https://habitica.com/api/v3',
-  request: (t, callback) => {
-    return h.withHeaders(t, headers => (
-      callback(
-        axios.create({
-          baseURL: h.api,
-          timeout: 10000,
-          headers: headers
-        })
-      )
+  request: (t, callback) => (
+    h.preventDuplicated(t, () => (
+      h.withHeaders(t, headers => (
+        callback(
+          axios.create({
+            baseURL: h.api,
+            timeout: 10000,
+            headers: headers
+          })
+        )
+      ))
     ))
-  },
-  withHeaders: (t, callback) => {
-    return t.get('member', 'private').then(member => (
+  ),
+  preventDuplicated: (t, callback) => (
+    t.card('id').get('id').then(card => {
+      if (h.loading[card.id]) { return }
+      
+      h.loading[card.id] = true
+      return callback().then(() => {
+        h.loading[card.id] = false
+      })
+    })
+  ),
+  withHeaders: (t, callback) => (
+    t.get('member', 'private').then(member => (
       callback({
         'x-api-user': member.userId,
         'x-api-key': member.apiToken,
         'Content-Type': 'application/json'
       })
     ))
-  },
+  ),
   addTodo: t => (
     h.request(t, (http) => (
-      t.card('name').then(card => {
+      t.card('name').then(card => (
         http.post('/tasks/user', {
           type: 'todo',
           text: card.name
         })
         .then((res) => res.data)
-        .then((res) => {
-          return t.set('card', 'private', {
+        .then((res) => (
+          t.set('card', 'private', {
             habiticaId: res.data.id
-          });
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-      })
+          })
+        ))
+        .catch(error => console.error(error))
+      ))
     ))
   ),
-  removeTodo: t => {
+  removeTodo: t => (
     h.request(t, (http) => (
       t.get('card', 'private').then(cardStorage => (
         http.delete(`/tasks/${cardStorage.habiticaId}`)
-          .then((res) => res.data)
-          .then((res) => {
-            return t.set('card', 'private', {
+          .then(_ => (
+            t.set('card', 'private', {
               habiticaId: null
             })
-          })
-          .catch((error) => {
-            console.error(error)
-          })
+          ))
+          .catch(error => console.error(error))
       ))
     ))
-  },
+  ),
   sync: t => (
     t.get('board', 'private', 'habiticaSyncedLists', {}).then(syncedLists => (
       t.card('id', 'idList').then(card => (
