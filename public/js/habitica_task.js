0e9478f3b8abfcf2dd86eadc5528096397d67b15
Alex Kutuzaki <alexktzk@gmail.com>
2019-01-28 18:57:52 +0300

    refactor: move tasks related code to separate class

---
+++ public/js/habitica_task.js
@@ -0,0 +1,105 @@
+class HabiticaTask {
+  constructor(trello) {
+    this.t = trello
+    this.API = new HabiticaApi(trello)
+    this.storage = new HabiticaStorage(trello)
+  }
+
+  template(card) {
+    let cardUrl = `https://trello.com/c/${card.shortLink}`
+
+    return {
+      type: 'todo',
+      text: `### ![](${TRELLO_ICON})&ensp; ${card.name}`,
+      notes: `[Open in Trello](${cardUrl})`
+    }
+  }
+
+  async handleAdd() {
+    let card = await this.t.card('name', 'shortLink')
+    let params = this.template(card)
+
+    return this.API.addTask(params)
+      .then(res => this.storage.setTask({ id: res.data.id }))
+  } 
+
+  async handleRemove() {
+    let task = await this.storage.getTask()
+
+    return this.API.removeTask(task.id)
+      .then(_ => this.storage.removeTask())
+  }
+
+  async handleDo() {
+    let task = await this.storage.getTask()
+
+    return this.API.doTask(task.id)
+      .then(_ => this.storage.setTask({ done: true }))
+  }
+
+  async handleUndo() {
+    let task = await this.storage.getTask()
+
+    return this.API.undoTask(task.id)
+      .then(_ => this.storage.setTask({ done: false }))
+  }
+
+  handleScopedSync(task) {
+    if (task.id) {
+      if (task.done) {
+        return this.handleUndo().then(() => this.handleRemove())
+      } else {
+        return this.handleRemove()
+      }
+    }
+  }
+
+  handleSync(task, listType) {
+    if (listType == LIST_TYPES.DOING) {
+      if (task.id) {
+        if (task.done) {
+          return this.handleUndo()
+        }
+      } else {
+        return this.handleAdd()
+      }
+    } else if (listType == LIST_TYPES.DONE) {
+      if (task.id) {
+        if (!task.done) {
+          return this.handleDo()
+        }
+      } else {
+        return this.handleAdd().then(() => this.handleDo())
+      }
+    } else {
+      if (task.id) {
+        if (task.done) {
+          return this.handleUndo().then(() => this.handleRemove())
+        } else {
+          return this.handleRemove()
+        }
+      }
+    }
+  }
+
+  async sync() {
+    let card = await this.t.card('id', 'idList', 'members')
+    let settings = await this.storage.getSettings()
+    let task = await this.storage.getTask()
+
+    if (settings.scope == CARD_SCOPES.ASSIGNED_TO_ME) { 
+      let me = this.t.getContext().member
+      if (!card.members.some(member => member.id == me)) {
+        return this.handleScopedSync(task)
+      }
+    }
+
+    let habiticaLists = await this.storage.getHabiticaLists()
+    let listType = habiticaLists[card.idList]
+
+    return this.handleSync(task, listType)
+  }
+}
+
+// Fails in a browser, but required for tests.
+try { module.exports = HabiticaTask } catch(_) {}
